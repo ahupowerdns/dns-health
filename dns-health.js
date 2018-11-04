01100650486d152e4bb2b257380f5374649bbbd0
bert hubert <bert.hubert@netherlabs.nl>
2018-11-04 13:02:44 +0100

    wip

--- dns-health.js
+++ dns-health.js
@@ -1,11 +1,29 @@
 'use strict';
 
-d3.select("#main").text("Calculating..");
+d3.select("#main").text("");
 
 var statuses={}
 var allsections={};
 var allrfcs={};
 var dnsrfcentries={};
+/**
+ * JavaScript Get URL Parameter
+ * 
+ * @param String prop The specific URL parameter you want to retreive the value for
+ */
+function getUrlParams( prop ) {
+    var params = {};
+    var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
+    var definitions = search.split( '&' );
+
+    definitions.forEach( function( val, key ) {
+        var parts = val.split( '=', 2 );
+        params[ parts[ 0 ] ] = parts[ 1 ];
+    } );
+
+    return ( prop && prop in params ) ? params[ prop ] : "";
+}
+
 
 function tabulate(data, columns) {
     d3.select('#table').html("");
@@ -30,16 +48,18 @@ function tabulate(data, columns) {
     var cells = rows.selectAll('td')
 	.data(function (row) {
 	    return columns.map(function (column) {
-		return {column: column, value: row[column], url: row["url"]};
+		return {column: column, value: row[column], url: row["url"], row: row};
 	    });
 	})
 	.enter()
 	.append('td')
 	.html(function (d) {
-            if(d.column != "title")
-                return d.value;
+            if(d.column == "name")
+                return '<a href="?showlog='+d.value+'">'+d.value+'</a>';
+            else if(d.column == "numqueries")
+                return '<a href="?showdot='+d.row["name"]+'">'+d.value+'</a>';
             else
-                return '<a href="'+d.url+'">'+d.value+'</a>';
+                return d.value;
         });
 
     return table;
@@ -108,22 +128,29 @@ function updateTable()
             arr.push(o);
         else if(statuses["Many queries"] && o.numqueries > 20)
             arr.push(o);
+        else if(statuses["Slow"] && o.msec > 3000)
+            arr.push(o);
         else if(statuses["Timeouts"] && o.numtimeouts > 1)
             arr.push(o);
         else if(statuses["EDNS Fallback"] && o.numformerrs > 0)
             arr.push(o);
-        
-        
     }
-    
-    tabulate(arr, ["name", "numqueries", "numtimeouts", "numformerrs", "rcode", "content"]); 
+    arr=arr.sort(function(a, b) {
+        if(a.name < b.name) return -1;
+        else if(b.name < a.name) return 1;
+        return 0;
+    });
+
+    tabulate(arr, ["name", "numqueries", "numtimeouts", "numformerrs", "msec", "rcode", "content"]); 
     
     d3.select("#main").text("There are "+Object.keys(dnsrfcentries).length + " domains, of which "+arr.length+" match criteria");
 }
 
 var results={};
 
-d3.json("result.json", {cache: "force-cache"}).then(function(js) {
+d3.json("result.json").then(function(js) {
+    var showdom = getUrlParams("showlog");
+    var showdot = getUrlParams("showdot");
     results = js;
     for(var a in js) {
         var dom = js[a];
@@ -132,12 +159,25 @@ d3.json("result.json", {cache: "force-cache"}).then(function(js) {
             dom.content = dom.content + dom.answer[b].content+ " ";
         }
         dnsrfcentries[dom.name] = dom;
+        if(dom.name == showdom) {
+            d3.select("#main").append("pre").text(dom.trace);
+            return;
+        }
+        else if(dom.name == showdot) {
+            d3.select("body").html("");
+            var tmp = d3.select("body").append("svg").style('width', '2048px');
+            tmp.style('height', '2048px');
+            
+            tmp.html(dom.dot);
+            return;
+        }
     }
     statuses["No Error"]=0;
     statuses["No Data"]=0;
     statuses["NXDOMAIN"]=1;
     statuses["SERVFAIL"]=1;
     statuses["Many queries"]=1;
+    statuses["Slow"]=1;
     statuses["Timeouts"]=1;
     statuses["EDNS Fallback"]=1;
     createTable();
